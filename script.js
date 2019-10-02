3af5fbe7789dd1935656d8e63c48ce226c4ee3cf
cis101 <cis101@secs.iub.edu.bd>
2019-10-02 09:59:07 +0600

    classwork5

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-/* JavaScript code here */
+var library = [ 
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games', 
+       readingStatus: false
+   }];
+
+console.log(//library[1].author
+
+  //getName (2)
+
+  );
+
+function getName (index){
+
+  return library[index].author
+
+}
+
